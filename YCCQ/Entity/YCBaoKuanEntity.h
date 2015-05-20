283c6ef388a908e5da083a96c527c4810502471d
Liu Feng <gumeit@gmail.com>
2015-05-20 15:25:42 +0800

    init

---
+++ YCCQ/Entity/YCBaoKuanEntity.h
@@ -0,0 +1,19 @@
+//
+//  YCBaoKuanEntity.h
+//  YCCQ
+//
+//  Created by Liu Feng on 15/5/19.
+//  Copyright (c) 2015年 Baisika. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YCBaoKuanEntity : NSObject
+
+@property (strong, nonatomic) NSString *imageName;
+
+@property (strong, nonatomic) NSString *series;
+
+@property (strong, nonatomic) NSString *price;
+
+@end
---
+++ YCCQ/Entity/YCBaoKuanEntity.m
@@ -0,0 +1,13 @@
+//
+//  YCBaoKuanEntity.m
+//  YCCQ
+//
+//  Created by Liu Feng on 15/5/19.
+//  Copyright (c) 2015年 Baisika. All rights reserved.
+//
+
+#import "YCBaoKuanEntity.h"
+
+@implementation YCBaoKuanEntity
+
+@end
