678577b448f6f8888fe4195b352f9efe5ebff478
Liu Feng <gumeit@gmail.com>
2015-05-20 15:25:50 +0800

    init

---
+++ YCCQ/View/YCBaoKuanCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  YCBaoKuanCollectionViewCell.h
+//  YCCQ
+//
+//  Created by Liu Feng on 15/5/19.
+//  Copyright (c) 2015年 Baisika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YCBaoKuanCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *carImageView;
+@property (weak, nonatomic) IBOutlet UILabel *carSeriesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *carPriceLabel;
+@end
---
+++ YCCQ/View/YCBaoKuanCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  YCBaoKuanCollectionViewCell.m
+//  YCCQ
+//
+//  Created by Liu Feng on 15/5/19.
+//  Copyright (c) 2015年 Baisika. All rights reserved.
+//
+
+#import "YCBaoKuanCollectionViewCell.h"
+
+@implementation YCBaoKuanCollectionViewCell
+
+@end
